7f820ce512987ae62352d5e2625c0061de718553
Ilya <bytskiy.i@gmail.com>
2024-08-03 00:42:05 +0300

    Updated object`s replacement

--- Assets/PlaceObj.cs
+++ Assets/PlaceObj.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -9,14 +10,23 @@ public class PlaceObj : MonoBehaviour
     public Material newMaterial;
     public Material oldMaterial;// Целевая область для перемещения
     public float speed = 1.0f;
+    private float delay = 2f;
     private bool isObjectSelected = false;
     private Vector3 targetPosition;
+    private GameObject desk = null;
+
+    private void Start()
+    {
+        desk = GameObject.Find("Desk");
+        
+    }
 
     void Update()
     {
         // Проверяем, был ли клик по объекту
         if (Input.GetMouseButtonDown(0))
         {
+            
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
@@ -39,6 +49,12 @@ public class PlaceObj : MonoBehaviour
                         //Debug.Log("tyu");
                         MoveToTargetArea(obj);
                     }
+                    else if (obj != null && obj.CompareTag("Draggable"))
+                    {
+                        
+                        slotSwap(gameObject, obj);
+                        
+                    }
                     isObjectSelected = false;
                     gameObject.GetComponent<Renderer>().material = oldMaterial;
                     // Если объект был выбран и клик произошел по целевой области
@@ -50,11 +66,12 @@ public class PlaceObj : MonoBehaviour
 
     void MoveToTargetArea(GameObject obj)
     {
+            Vector3 targetPos = obj.transform.position;
+            //transform.rotation = obj.transform.rotation;
         // Перемещаем объект к целевой области
-        while (transform.position != obj.transform.position)
+        while (transform.position != targetPos)
         {
-            
-            transform.position = Vector3.MoveTowards(transform.position, obj.transform.position, speed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         }
          // Сбрасываем выбор объекта
 
@@ -63,24 +80,61 @@ public class PlaceObj : MonoBehaviour
     private GameObject findObj()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        RaycastHit[] hits;
         GameObject clickedObject = null;
+        GameObject slotObject = null;
 
         // Выполнение Raycast и проверка, попал ли луч в коллайдер объекта
-        if (Physics.Raycast(ray, out hit))
+        if (Physics.Raycast(ray))
         {
+            hits = Physics.RaycastAll(ray);
             // Получение объекта, в который попал луч
-            clickedObject = hit.collider.gameObject;
-            if (clickedObject.CompareTag("Draggable")) return null;
-            //Debug.Log("Clicked on: " + clickedObject.name + " is " + taken_slot._objects[clickedObject]);
-            if (taken_slot._objects[clickedObject] == false) taken_slot._objects[clickedObject] = true;
-            else if (taken_slot._objects[clickedObject] == true) return null;
-            Debug.Log("Clicked on: " + clickedObject.name);
-
-            // Дальнейшие действия с объектом
-            // Например, можно выделить объект или применить к нему эффект
+            clickedObject = hits[0].collider.gameObject;
+            if (!clickedObject.CompareTag("Destinations")) return null;
+            slotObject = hits[1].collider.gameObject; 
+            //Debug.Log(clickedObject + " " + slotObject + " ");
+            if (taken_slot._objects.ContainsKey(clickedObject) && slotObject == desk)
+            {
+                if (checkDesk())
+                {
+                    return clickedObject;
+                }
+                if (taken_slot._objects[clickedObject] == false) taken_slot._objects[clickedObject] = true;
+                else if (taken_slot._objects[clickedObject] == true) return null;
+            }
+            else 
+            {
+                if (checkDesk()) {
+                    return slotObject;
+                }
+            }
         }
 
         return clickedObject;
+    }
+
+    private void slotSwap(GameObject original, GameObject replaceable)
+    {
+        Vector3 tempPosition = original.transform.position;
+        Transform tempParent = original.transform.parent;
+
+        // Перемещаем obj1 на место obj2
+        original.transform.position = replaceable.transform.position;
+        original.transform.parent = replaceable.transform.parent;
+
+        // Перемещаем obj2 на место obj1
+        replaceable.transform.position = tempPosition;
+        replaceable.transform.parent = tempParent;
+        Debug.Log(original.name + " " + replaceable.name);
+        
+        //MoveToTargetArea(original);
+    }
+
+    private bool checkDesk()
+    {
+        Bounds descBounds = desk.GetComponent<Renderer>().bounds;
+
+        return (transform.position.x >= descBounds.min.x && transform.position.x <= descBounds.max.x &&
+            transform.position.z >= descBounds.min.z && transform.position.z <= descBounds.max.z) ;
     }
 }
